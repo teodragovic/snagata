2bfc5afd4edf28c9643df911d8fe9a7983a42766
Teo Dragovic <teo.dragovic@gmail.com>
2016-05-27 22:17:55 +0200

    Site updated at 2016-05-27 20:17:53 UTC

--- sw.js
+++ sw.js
@@ -4,14 +4,15 @@
 (function() {
 
     const staticCacheName = 'static';
-    const version = 'v21::';
+    const version = 'v22::';
 
     const urlsToCache = [
         '/snagata/',
         '/snagata/index.html',
         '/snagata/index.html?homescreen',
         '/snagata/css/main.css?v=5',
-        '/snagata/scripts/main.js?v=1'
+        '/snagata/scripts/main.js?v=1',
+        '/snagata/offline/'
     ];
 
     const imgPlaceholder = '<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>';
@@ -19,7 +20,7 @@
     function updateStaticCache() {
         return caches.open(version + staticCacheName)
             .then(cache => cache.addAll(urlsToCache));
-    };
+    }
 
     function putToCache(request, response) {
         if (response.ok) {
@@ -30,13 +31,13 @@
                 });
         }
         return response;
-    };
+    }
 
     function doesRequestAcceptHtml(request) {
         return request.headers.get('Accept')
             .split(',')
             .some(type => type === 'text/html');
-    };
+    }
 
     function unableToResolve(request) {
         const accepts = request.headers.get('Accept');
@@ -46,7 +47,7 @@
             return offlineResponse();
         }
         return undefined;
-    };
+    }
 
     function offlineResponse() {
         return new Response('<h1>Service Unavailable</h1>', {
@@ -54,9 +55,9 @@
             statusText: 'Service Unavailable',
             headers: new Headers({ 'Content-Type': 'text/html' })
         });
-    };
+    }
 
-    self.addEventListener('install', function(event) {
+    self.addEventListener('install', event => {
         event.waitUntil(
             updateStaticCache()
             .then(() => self.skipWaiting())
@@ -64,7 +65,7 @@
     });
 
 
-    self.addEventListener('activate', (event) => {
+    self.addEventListener('activate', event => {
         function onActivate() {
             return caches.keys()
                 .then(cacheKeys => {
@@ -82,7 +83,7 @@
 
 
 
-    self.addEventListener('fetch', (event) => {
+    self.addEventListener('fetch', event => {
 
         let request = event.request;
         const url = new URL(request.url);
