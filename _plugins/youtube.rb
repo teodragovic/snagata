f1689c5d5d73163850ea3375150eada4c16a1e3f
Teo Dragovic <teo.dragovic@gmail.com>
2016-05-29 00:19:52 +0200

    Implement AMP

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -9,7 +9,7 @@ class YouTube < Liquid::Tag
 
       if $2.nil? then
           @width = 560
-          @height = 420
+          @height = 315
       else
           @width = $2.to_i
           @height = $3.to_i
@@ -19,9 +19,20 @@ class YouTube < Liquid::Tag
     end
   end
 
+  def lookup(context, name)
+    lookup = context
+    name.split(".").each { |value| lookup = lookup[value] }
+    lookup
+  end
+
   def render(context)
-    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"https://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+    is_amp = lookup(context, 'page.amp')
+    if is_amp.nil? then
+      output = "<p class=\"youtube-embed\"><iframe width=\"#{@width}\" height=\"#{@height}\" src=\"https://www.youtube.com/embed/#{@id}\" frameborder=\"0\" allowfullscreen></iframe></p>"
+    else
+      output = "<amp-youtube data-videoid=\"#{@id}\" layout=\"responsive\" width=\"480\" height=\"270\"></amp-youtube>"
+    end
+    output
   end
 
   Liquid::Template.register_tag "youtube", self
